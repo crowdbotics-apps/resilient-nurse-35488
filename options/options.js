f1395b334a9a4f0b9bc724d38d4cb65e76b7f817
Crowdbotics <team@crowdbotics.com>
2022-05-28 18:15:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "resilient_nurse_35488",
+  url: "https://resilient_nurse_35488.botics.co",
+  api: "https://resilient_nurse_35488.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
